e9b36d518347f09ef8784f10abfb2bce8bdabcd2
Aryan730-wb <aryankd730@gmail.com>
2022-10-08 16:27:58 +0100

    First Commit

---
+++ build/precache-manifest.675e5376eaf6d38608e85f1a916f9270.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "522dc997febdbc2334b0",
+    "url": "/static/css/main.35f89d68.chunk.css"
+  },
+  {
+    "revision": "522dc997febdbc2334b0",
+    "url": "/static/js/main.522dc997.chunk.js"
+  },
+  {
+    "revision": "90ecf6b5710b2396cef7",
+    "url": "/static/js/1.90ecf6b5.chunk.js"
+  },
+  {
+    "revision": "81fb44f5ec5921ec6c7c",
+    "url": "/static/css/2.be4b59d9.chunk.css"
+  },
+  {
+    "revision": "81fb44f5ec5921ec6c7c",
+    "url": "/static/js/2.81fb44f5.chunk.js"
+  },
+  {
+    "revision": "f63f43421bdc937aa0f2",
+    "url": "/static/js/runtime~main.f63f4342.js"
+  },
+  {
+    "revision": "08e64b2e73b4210f4b66450f3503dfac",
+    "url": "/static/media/logo1.08e64b2e.png"
+  },
+  {
+    "revision": "7153f61f03be5fa712ddb0fb51bbe6cc",
+    "url": "/static/media/netflix1.7153f61f.png"
+  },
+  {
+    "revision": "5fbc6bcbab24468a8e9d8c4f10039425",
+    "url": "/index.html"
+  }
+];
